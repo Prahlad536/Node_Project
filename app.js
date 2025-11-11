e97cb0fea2b0fc88faca7813c7c81b2f0155ed30
Prahlad <majhirn320@gmail.com>
2025-11-11 20:27:20 +0545

    learning about validation, read operation

--- app.js
+++ app.js
@@ -17,8 +17,9 @@ app.use(express.urlencoded({extended: true}))
 app.use(express.json())
 
 // Home
-app.get("/home", (get, res)=>{
-    res.render("home.ejs")
+app.get("/home",async (get, res)=>{
+    const userTableUser= await users.findAll()
+    res.render("home.ejs", {users: userTableUser})
 })
 
 // app.get("/about", (req, res)=>{
@@ -33,6 +34,10 @@ app.get("/adblog", (req, res)=>{
 app.post("/adblog",async (req, res)=>{
     const {title, subTitle, description}= req.body
     console.log(title, subTitle,description)
+    //server validation
+     if(!title || !subTitle || !description){
+        return res.send("Please Provide titile, subtitle and description")
+     }
 
       //inserting into blogs tables
      await blogs.create({
@@ -50,17 +55,24 @@ app.get("/user", (req, res)=>{
     res.render("user.ejs")
 })
 
+// Post Route
 app.post("/user", async(req, res)=>{
     const {name, email, password}=req.body
     console.log(name, email, password)
 
+    //server validation
+    if(!name || !email || !password){
+        return res.send("please fill up the details")
+    }
+
     //inserting into users tables
+    //database ma table ma halney method
     await users.create({
              name: name,
              email: email,
              password: password
     })
-    res.send("Register Success Fully!!!")
+    res.redirect("/home")
        
 })
 
